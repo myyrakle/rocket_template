fb21622c486eb2b2cf02b0e5bd20d772ddd5f419
myyrakle <sssang97@naver.com>
2020-02-05 16:26:18 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,13 @@ fn index() -> Template
     Template::render("index", &context)
 }
 
+use rocket_contrib::serve::StaticFiles;
+
 fn main() 
 {
     rocket::ignite()
         .mount("/", routes![index])
+        .mount("/static", StaticFiles::from("static"))
         .attach(Template::fairing())
         .launch();
 }
